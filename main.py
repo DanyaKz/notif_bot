7ff64325cff5db0d1c935fe10e54183ca2429e05
Danya Zverev <i_am_zver@yahoo.com>
2024-01-10 06:56:58 +0600

    fix

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from middleware import SimpleLoggerMiddleware
 from aiogram import Router
 from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, Command, ChatMemberUpdatedFilter
 from aiogram.handlers import MessageHandler
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 API_TOKEN = '6630774756:AAFTsxAbj7uM6Woz5a7BtWkMXcrEVd6Gdt8'
 DB_CONFIG = {
@@ -16,33 +20,37 @@ DB_CONFIG = {
             'port': 5432,    
             'user': 'ydcclkyluxxblw',
             'password': '10984c6b9c775b1020d496f434fb20b3f2d7bb7dd590774c4cde22c513fcdb24',    
-            'database': 'd7kb3aae5a9311',}
+            'database': 'd7kb3aae5a9311'
+            }
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+pool = None
 router = Router()
-router.message.middleware(SimpleLoggerMiddleware())
-dp.include_router(router)
+dp = Dispatcher()
+bot = Bot(token=API_TOKEN)
+
 
 
 async def get_notifications():
-    conn = await asyncpg.connect(**DB_CONFIG)
-    try:
-        notifications = await conn.fetch("""
-            SELECT m.id, m.deadline, m.message_text, g.chat_id, m.num_of_notif 
-            FROM notificator.messages m
-            JOIN notificator.notifications_groups ng ON ng.notif_id = m.id
-            RIGHT JOIN notificator.student_groups g ON g.id = ng.group_id
-            WHERE m.is_relevant = 1
-            ORDER BY m.id DESC;
-        """)
-    finally:
-        await conn.close()
-    return notifications
+    async with pool.acquire() as conn:
+        try:
+            notifications = await conn.fetch("""
+                SELECT m.id, m.deadline, m.message_text, g.chat_id, m.num_of_notif 
+                FROM messages m
+                JOIN notifications_groups ng ON ng.notif_id = m.id
+                RIGHT JOIN student_groups g ON g.id = ng.group_id
+                WHERE m.is_relevant = true
+                ORDER BY m.id DESC;
+            """)
+            return notifications
+        except Exception as e:
+            print(f"Error in set_group_name: {e}")
 
 
 async def send_notifications():
     notifications = await get_notifications()
+    if notifications == None:
+        return
+
     tz_utc_plus_6 = pytz.timezone('Asia/Almaty')
     now = datetime.now(tz=tz_utc_plus_6)
     for notification in notifications:
@@ -64,20 +72,17 @@ async def send_notifications():
 
 
 async def mark_notification(notification, is_relevant = True):
-    pool = await asyncpg.create_pool(**DB_CONFIG)
     async with pool.acquire() as conn:
         try:
             await conn.execute(
                 """
-                UPDATE notificator.messages 
+                UPDATE messages 
                 SET num_of_notif = $1, is_relevant = $2 WHERE id = $3;
                 """,
                 notification['num_of_notif'] + 1, is_relevant, notification['id']
             )
         except Exception as e:
             print(f"Error updating data: {e}")
-        finally:
-            await pool.close()
     
 
 @router.my_chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
@@ -117,28 +122,24 @@ If you want to change its name, modify the group name, then send the /rename com
 @router.my_chat_member(ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER))
 async def on_user_leave(event: ChatMemberUpdated):
     bot_user_id = bot.id 
-    pool = await asyncpg.create_pool(**DB_CONFIG)
     async with pool.acquire() as conn:
         if event.old_chat_member.user.id == bot_user_id:
             try:
                 await conn.execute(
                     """
                     UPDATE student_groups 
-                    SET is_available = 0 
+                    SET is_available = false 
                     WHERE chat_id = $1;
                     """,
                     event.chat.id
                 )
             except Exception as e:
                 print(f"Error updating data: {e}")
-            finally:
-                await pool.close()
         
 
 
 
 async def set_group_name(group_name, chat_id):
-    pool = await asyncpg.create_pool(**DB_CONFIG)
     async with pool.acquire() as conn:
         existing_group = await conn.fetchrow(
             "SELECT * FROM student_groups WHERE chat_id = $1", chat_id
@@ -147,7 +148,7 @@ async def set_group_name(group_name, chat_id):
             await conn.execute(
                 """
                 UPDATE student_groups 
-                SET group_name = $1, is_available = 1 
+                SET group_name = $1, is_available = true 
                 WHERE chat_id = $2
                 """,
                 group_name, chat_id
@@ -160,7 +161,6 @@ async def set_group_name(group_name, chat_id):
                 """,
                 chat_id, group_name
             )
-        await pool.close()
 
 
 
@@ -187,12 +187,28 @@ If you want to change its name, modify the group name, then send the /rename com
 async def scheduler():
     while True:
         await send_notifications()
-        await asyncio.sleep(10)
+        await asyncio.sleep(1200)
 
 
-async def on_startup(_):
-    asyncio.create_task(scheduler())
+async def on_startup(dispatcher):
+    try:
+        logging.info("Starting application...")
+        global pool
+        pool = await asyncpg.create_pool(**DB_CONFIG)
+        logging.info("Database pool initialized.")
+        asyncio.create_task(scheduler())
+        logging.info("Application successfully started.")
+    except Exception as e:
+        print(f"Error during startup: {e}")
 
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True) 
+    router.message.middleware(SimpleLoggerMiddleware())
+    dp.include_router(router)
+    dp.startup.register(on_startup)
+    
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == '__main__':
-    asyncio.run(dp.start_polling(bot, on_startup=on_startup))
+    asyncio.run(main())
